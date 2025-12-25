1465abd30b2b5f34689273100804056234b0db78
root <root@sarauwu.localdomain>
2025-12-25 14:10:49 +0200

    Implementarea functiei add in main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
 import json
 import sys
 
-FILE_PATH = "lista_de_cumparaturi.json"
+ command = sys.argv[1]
+
+        if command == "add":
+                if len(sys.argv) !=6:
+                        print("Folosire: add <nume> <cantitate> <pret> <categorie>")
+                        return
+
+                nume = sys.argv[2]
+                cantitate = int(sys.argv[3])
+                pret = float(sys.argv[4])
+                categorie = sys.argv[5]
+
+                add_item(nume, cantitate, pret, categorie)
+        else:
+                print(f"Comanda introdusa nu exista: {command}")
+if __name__ ==  "__main__":
+
+        main()FILE_PATH = "lista_de_cumparaturi.json"
 
 def load_items():
 	try:
@@ -29,23 +46,39 @@ def add_item(nume, cantitate, pret, categorie):
 		"cantitate": cantitate,
 		"pret": pret,
 		"categorie": categorie
-}
+	}
+
 	items.append(item)
-	save.items(items)
+	save_items(items)
 
 	total = cantitate * pret
 
 	print(
-		f"Articol adaugat cu succes: {name} "
-		f"(cantitate: {cantitate}, pret: {pret}, total: {total}, categorie: {categorie})
-)
+		f"Articol adaugat cu succes: {nume} "
+		f"(cantitate: {cantitate}, pret: {pret}, total: {total}, categorie: {categorie})"
+	)
 
 def main():
-	items = load_items()
-	print("Lista curenta de cumparaturi:")
-	print(items)
+	if len(sys.argv) < 2:
+		print("Folosire: python3 app/main.py add <nume> <cantitate> <pret> <categorie>")
+		return
+
+	command = sys.argv[1]
+
+	if command == "add":
+		if len(sys.argv) !=6:
+			print("Folosire: add <nume> <cantitate> <pret> <categorie>")
+			return
+
+		nume = sys.argv[2]
+		cantitate = int(sys.argv[3])
+		pret = float(sys.argv[4])
+		categorie = sys.argv[5]
+
+		add_item(nume, cantitate, pret, categorie)
+	else:
+		print(f"Comanda introdusa nu exista: {command}")
 
 if __name__ ==  "__main__":
-
 	main()
 
